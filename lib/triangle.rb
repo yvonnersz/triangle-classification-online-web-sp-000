b56ca39a07538d3356be38c1e235ce8ba115fc67
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-10 23:48:46 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,14 @@
 class Triangle
-  # write code here
+  attr_accessor :length_1, :length_2, :length_3
+
+  def initialization(length_1, length_2, length_3)
+    @length_1 = length_1
+    @length_2 = length_2
+    @length_3 = length_3
+  end
+
+
+
+
+
 end
