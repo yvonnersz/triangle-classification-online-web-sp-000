0a98f55854c0762a346a1c795cb02082bd3cb6f6
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-11 02:13:08 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,14 +1,30 @@
-class Triangle
-  attr_accessor :length_1, :length_2, :length_3
+require 'pry'
 
-  def initialization(length_1, length_2, length_3)
-    @length_1 = length_1
-    @length_2 = length_2
-    @length_3 = length_3
+class Triangle
+  attr_accessor :a, :b, :c
+
+  def initialize(a,b,c)
+    @a = a
+    @b = b
+    @c = c
+    @lengths = [a,b,c]
   end
 
+  def kind
+    @lengths.sort
+    raise TriangleError if @lengths.any? {|length| length <= 0}
+    raise TriangleError if (@lengths.sort[0] + @lengths.sort[1] <= @lengths.sort[2])
 
+    if @lengths.uniq.count == 1
+      :equilateral
+    elsif @lengths.uniq.count == 2
+      :isosceles
+    else
+      :scalene
+    end
+  end
 
-
+  class TriangleError < StandardError
+  end
 
 end
